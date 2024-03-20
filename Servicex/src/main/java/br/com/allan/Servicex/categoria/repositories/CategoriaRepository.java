cf5ab31419182f86d160f74a1d856aaf6da6405f
AllanGomes09 <115575030+allangomes09@users.noreply.github.com>
2024-03-20 18:08:18 -0300

    commit 1

---
+++ Servicex/src/main/java/br/com/allan/Servicex/categoria/repositories/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package br.com.allan.servicex.categoria.repositories;
+
+import br.com.allan.servicex.categoria.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+}
